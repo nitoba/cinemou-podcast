fe906cb3e8b16232b97792ec63fe35384c356f6a
Bruno <bruno@sthorm.io>
2023-09-22 23:08:52 -0300

    chore: add api endpoint

---
+++ src/pages/api/user/[id].ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from 'astro';
+
+export const prerender = false;
+
+export const GET: APIRoute = async ({ params, request }) => {
+    const id = params.id;
+
+    return new Response(
+      JSON.stringify({
+        name: { id }
+      })
+    )
+}
